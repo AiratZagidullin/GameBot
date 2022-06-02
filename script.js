63bb1fdc4a35d57dff44302ce6476a7b688af92f
Airat Zagidullin <zagbomar@gmail.com>
2022-06-02 19:37:47 +0500

    Выполнил усложненное задание 06

--- script.js
+++ script.js
@@ -7,22 +7,38 @@ const isNumber = function (num) {
 const gameBot = function () {
     let randomNum = parseInt(Math.random() * 100);
     let userNum = prompt('Угадай число от 1 до 100');
+    let attempt = 10;
 
     const guessNumber = function () {
-        if (userNum === null) {
+        if (userNum === null || userNum === false) {
             alert('Игра окончена')
-            return userNum
+            return userNum()
         } else if (isNumber(userNum)) {
             userNum = +userNum;
+            attempt--;
 
-            if (userNum > randomNum) {
-                userNum = prompt('Загаданное число меньше');
-            } else if (userNum < randomNum) {
-                userNum = prompt('Загаданное число больше');
-            } else if (userNum === randomNum) {
-                alert('Поздравляю, Вы угадали!!!');
-                return userNum;
+            if (attempt > 0) {
+                if (userNum > randomNum) {
+                    userNum = prompt('Загаданное число меньше, осталось попыток ' + attempt);
+                } else if (userNum < randomNum) {
+                    userNum = prompt('Загаданное число больше, осталось попыток ' + attempt);
+                } else if (userNum === randomNum) {
+                    userNum = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+                    if (userNum === true) {
+                        return gameBot();
+                    } else {
+                        return guessNumber();
+                    }
+                }
+            } else {
+                userNum = confirm('Попытки закончились, хотите сыграть еще?')
+                if (userNum === true) {
+                    return gameBot();
+                } else {
+                    return guessNumber();
+                }
             }
+
         } else {
             userNum = prompt('Введи число!');
         }
